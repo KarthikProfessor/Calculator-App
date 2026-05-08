c5ad5ee5fca75e84d6b760a3257d448e4f8839ee
KarthikKonduru <156228750+KarthikProfessor@users.noreply.github.com>
2026-05-08 17:50:00 +0530

    Added a comment

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function appendToDisplay(input) {
   display.value += input;
 }
 
+// eval --> it is a mini calculator built-in func
+// syntax: eval(value);
 function calculate() {
   try {
     display.value = eval(display.value);
